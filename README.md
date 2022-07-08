f87c028fa6c3bebbcf161b69127e7afd372eeb92
APURV CHOUBEY <apurv.main2@gmail.com>
2022-07-08 20:36:37 +0530

    coded the game part

--- README.md
+++ README.md
@@ -1,1 +1,42 @@
 # Webiste Link - Work on progress
+
+# Introduction
+This is a basic game to release stress and have some
+fun while clicking on the mouse and see your results
+on the boxes on the screen.
+
+# Documentation
+
+# Structure
+ 
+## HTML
+    1. The html page is simple with heading and three
+       boxes with red, yellow and green colors and 
+       also with a reset button which clears all
+       your current scores.
+
+## Javascipt
+    1. Take all the buttons of coloured boxes with
+       document.querySelectorAll. After this we will 
+       get 3 <button> html.
+    
+    2. We will loop through forEach for all the <button>
+       html we got to get the value like 'red', 'green', 
+       and 'yellow'.
+    
+    3. We will make and object with its keys as 'red',
+       'green', and 'yellow' and set their values as 0
+       in the start.
+    
+    4. The main logic is when we click on the boxes the
+       onclick function will give the value of that 
+       button as 'red' fo example, and we have made 
+       'red' as our key already so we will increment 
+       object.value by 1. In this example before::{'red': 0},
+       after::{'red': 1} and add that 1 by innerText to button.
+
+    5. AFter that we made a rest button where in 
+       all the object.value will be 0 and we will
+       run a for loop for all three boxes by forEach
+       to not show 0 in the box but to set '' empty
+       string so it looks good.
--- boxex.js
+++ boxex.js
@@ -1,1 +1,36 @@
-console.log("box is herre");
+// selecting all three boxes, i.e. red yellow and greeen 
+let boxesColor = document.querySelectorAll(".boxesColor");
+
+// making an object to display the no. of time clicked inside the box
+const timesClicked = { 'red': 0, 'yellow': 0, 'green': 0 };
+
+/*  since 'boxesColor' has all the <button> html, so we have to get the value of each html
+    and we have created the timesClicked object with the same names given as value in html
+    we will get and thus increase the value like red-0, red-1, red-2 by clicking on the box
+    and with the help of innerText we diplay the object.value in the box itself dynamically
+*/
+boxesColor.forEach(getBoxColor => {
+    getBoxColor.onclick = () => {
+        timesClicked[getBoxColor.value]++;
+        getBoxColor.innerText = timesClicked[getBoxColor.value];
+    }
+});
+
+/*  function to clear the value to 0 in each box and we will not show 0 so we have set it to ''
+    we have used loop because there are 3 boxes and we are looping to set '' in each boxes rather
+    than hard coding it
+*/
+function clearScores() {
+    timesClicked.red = 0
+    timesClicked.yellow = 0
+    timesClicked.green = 0
+    boxesColor.forEach(square => {
+        square.innerText = ''
+    })
+}
+
+// on clicking the reset button the clear function is called to reset all the values
+const resetGameBtn = document.getElementById('resetBtn')
+resetGameBtn.onclick = () =>{
+    clearScores();
+}
